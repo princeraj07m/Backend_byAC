ebd9fc09a3b27d66b5ff381f6ef61b451b114283
Prince kumar <princerajm31@gmail.com>
2025-06-15 13:59:13 +0530

    Update index.js

--- Backend/Express/index.js
+++ Backend/Express/index.js
@@ -4,7 +4,6 @@ const app = express();
 let port = 3000;
 
 
-//app.use is deafault reponse for all request if no route to it.
 // app.use((req,res) => {
 //     console.log("Request received ! ");
 //     res.send('Hello World1');
@@ -22,7 +21,7 @@ app.get('/', (req, res) => {
 app.get("/search", (req, res) => {
     let { q } = req.query;
     if(!q){
-        res.send("Nothing searched")
+        res.send("Nothing is searched")
     }
     res.send(`Hello you searched ${q}`);  // we can send plain text , html code , variable or JSON.
   });
